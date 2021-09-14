1cc384a2473cd629ac6c8ced013898b98719d21f
jcjobs <jcarlosp.ceo@gmail.com>
2021-09-14 14:26:20 -0500

    Initial Commit

---
+++ GraphQLPagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraphQLPagination
+//
+//  Created by Juan Carlos Perez Delgadillo on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
